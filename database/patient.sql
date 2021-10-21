69d2599363393da9bb19560a580852745135d2b6
anhquanqueo25 <aqdz01@gmail.com>
2021-10-21 15:04:35 +0700

    Ho Hong Quan 21/10/2021 15:04
    database

--- database/patient.sql
+++ database/patient.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 21, 2021 lúc 08:15 AM
+-- Thời gian đã tạo: Th10 21, 2021 lúc 10:03 AM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 8.0.10
 
@@ -37,10 +37,20 @@ CREATE TABLE `patient` (
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `height` float NOT NULL,
   `weight` float NOT NULL,
-  `blood_type` int(11) NOT NULL,
+  `blood_type` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_on` date NOT NULL,
   `modified_on` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `patient`
+--
+
+INSERT INTO `patient` (`patientid`, `firstname`, `lastname`, `dateofbirth`, `gender`, `mobile`, `email`, `height`, `weight`, `blood_type`, `created_on`, `modified_on`) VALUES
+(1, 'Quân', 'Hồ Hồng       ', '2001-11-25', '       Nam', '0346785893', 'aqdz01@gmail.com', 1.64, 55, 'AB', '2021-08-12', '2021-10-07'),
+(3, 'Vương', 'Nguyễn Minh ', '2001-07-25', ' Nam', ' 034324234', ' vuong@gmail.com', 1.65, 55, 'O', '2021-10-19', '2021-10-03'),
+(4, 'Tân', 'Nguyễn Văn Tân', '2001-03-02', 'Nam', ' 0342424324', ' tan@gmail.com', 1.64, 55, 'O', '2021-10-04', '2021-10-25'),
+(5, 'Hiếu ', 'Hồ Đức', '2001-01-30', 'Nam', ' 0335785981', ' hieu@gmail.com', 1.7, 59, 'O', '2021-10-06', '2021-10-25');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -60,7 +70,7 @@ ALTER TABLE `patient`
 -- AUTO_INCREMENT cho bảng `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `patientid` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `patientid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
